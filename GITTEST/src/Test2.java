a941c18874969f0d928edb7dc4b8bf5639eb1e40
tilak <tilak.chugh@gmail.com>
2016-10-22 16:46:16 +0530

    add a simple output change

--- GITTEST/src/Test2.java
+++ GITTEST/src/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testing git");
-
+		System.out.println("Testing new commit");
 	}
 
 }
