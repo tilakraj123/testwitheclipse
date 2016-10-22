4a55a2409f26fe02de4d6997ba8852764df8cfe1
tilak <tilak.chugh@gmail.com>
2016-10-22 18:00:30 +0530

    changing 2 files

--- GITTEST/src/Test2.java
+++ GITTEST/src/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testing git");
-		System.out.println("removing print statement");
+		System.out.println(" print statement for test2");
 	}
 
 }
